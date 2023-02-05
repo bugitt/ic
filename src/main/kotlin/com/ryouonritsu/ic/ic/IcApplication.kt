09bcf3348a7ab119b9a8e4491fa491318bb76bf1
RyouonRitsu <zhou.hongxi0903@qq.com>
2023-02-06 02:51:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/ryouonritsu/ic/ic/IcApplication.kt
@@ -0,0 +1,11 @@
+package com.ryouonritsu.ic.ic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IcApplication
+
+fun main(args: Array<String>) {
+    runApplication<IcApplication>(*args)
+}
---
+++ src/test/kotlin/com/ryouonritsu/ic/ic/IcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ryouonritsu.ic.ic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
